5a6f50f85b7aab648ff524c48e8db944e9acd40b
Joga <joga.gavidia@gmail.com>
2020-04-23 23:37:07 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $(function() {
     $('.flexslider').flexslider({
       animation: "fade"
     });
+
+    $('.owl-theme').owlCarousel({
+      items: 4,
+      margin:15,
+      autoplay:true
+      })
+
+
   });
